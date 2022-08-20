406bd136171e268bad8e9baa60f7cac3eefd1fdb
Brooks Lybrand <lybrand.brooks@heb.com>
2022-08-20 16:39:44 -0500

    Add random word generator to test caching loader results

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,38 @@
-import type { HeadersFunction } from "@remix-run/cloudflare";
-import { Link } from "@remix-run/react";
+import { json } from "@remix-run/cloudflare";
+import { Link, useLoaderData } from "@remix-run/react";
+import randomWords from "random-words";
 
-export const headers: HeadersFunction = () => {
-  return {
-    "Cache-Control": "max-age=300, s-maxage=600",
-  };
-};
+// export const headers: HeadersFunction = () => {
+//   return {
+//     "Cache-Control": "private, max-age=10, s-maxage=600",
+//   };
+// };
+
+export async function loader() {
+  await new Promise((res) => setTimeout(res, 2000));
+
+  const [word] = randomWords(1);
+
+  return json(
+    { word },
+    {
+      headers: {
+        // max-age controls the browser cache
+        // s-maxage controls a CDN cache
+        "Cache-Control": "public, max-age=30, s-maxage=86400",
+      },
+    }
+  );
+}
 
 export default function Index() {
+  const { word } = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix, yo</h1>
+      <h1>Welcome to Remix</h1>
       <Link to="/test">Go to test</Link>
+      <h2>The word was: {word}</h2>
     </div>
   );
 }
