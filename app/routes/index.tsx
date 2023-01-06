2c57acb2a7becbc11415af0d5e50a56057b1cbb2
Brooks Lybrand <brookslybrand@gmail.com>
2023-01-06 09:37:54 -0600

    Display the data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck TODO: add validation
+
 import { json } from "@remix-run/cloudflare";
 import type { LoaderArgs } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
@@ -15,35 +17,38 @@ export async function loader({ context }: LoaderArgs) {
 
 export default function Index() {
   const { results } = useLoaderData<typeof loader>();
-  console.log("the results", results);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix with React 18 babee</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>
+        Remix +{" "}
+        <a
+          href="https://developers.cloudflare.com/d1/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Cloudflare D1!
+        </a>
+      </h1>
+      <table>
+        <thead>
+          <tr>
+            {Object.keys(results[0]).map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {results?.map((result) => (
+            <tr key={result.CustomerID}>
+              {Object.values(result).map((value) => (
+                <td key={value}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
