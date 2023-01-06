f535e622b424bcff30d0c423de61a0f33b93d6f7
Brooks Lybrand <brookslybrand@gmail.com>
2023-01-06 09:10:18 -0600

    Testing D1

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,21 @@
+import { json } from "@remix-run/cloudflare";
+import type { LoaderArgs } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({ context }: LoaderArgs) {
+  const db = context.DB as D1Database;
+
+  const { results } = await db
+    .prepare("SELECT * FROM Customers WHERE CompanyName = ?")
+    .bind("Bs Beverages")
+    .all();
+
+  return json({ results });
+}
+
 export default function Index() {
+  const { results } = useLoaderData<typeof loader>();
+  console.log("the results", results);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix with React 18 babee</h1>
